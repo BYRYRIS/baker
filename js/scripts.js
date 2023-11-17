625d967d47a57f0baf8b3ed84505f64a100e58dc
Your Name01~ <you@example.com>
2023-11-17 17:24:07 +0200

    added adaptive

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
-document.querySelector(".order-call").addEventListener("click", hideOff);
+document.querySelector(".js-order-call").addEventListener("click", hideOff);
 
-document.querySelector(".modal-button").addEventListener("click", hideOn);
+document.querySelector(".js-modal-button").addEventListener("click", hideOn);
 
 // add/remove classes
 function hideOn() {
@@ -9,4 +9,16 @@ function hideOn() {
 
 function hideOff() {
     document.querySelector(".blackdrop").classList.toggle("hideModel");
+}
+
+document.querySelector(".js-mobile-menu-open").addEventListener("click", menuOn);
+
+function menuOn() {
+    document.querySelector(".mobile-menu__container").classList.toggle("mobile-menu__hider");
+}
+
+document.querySelector(".js-mobile-menu-cancel").addEventListener("click", menuOff);
+
+function menuOff() {
+    document.querySelector(".mobile-menu__container").classList.toggle("mobile-menu__hider");
 }
